6a36c59def0810d4ee37576d44db48877fb38655
ncutixavier <ncuti60@gmail.com>
2022-02-02 23:59:51 +0200

    resolve undefined issue

--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import multer from 'multer';
-import { UserControllers } from '../../controllers/userController.js';
+import { UserControllers } from '../../controllers/UserController.js';
 import { fileFilter } from '../../helpers/fileFilter.js';
 import { userValidation } from '../../validations/userValidation/user.validation.js';
 
